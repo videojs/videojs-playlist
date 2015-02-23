87a65a688ebed998bdae243a5376f7080c766e95
Gary Katsevman <git@gkatsev.com>
2015-02-23 14:32:33 -0500

    Playlist plugin with initial functionality This currently contains next, previous, list, and item. next: play next item. previous: play previous item. list: get the current list. item: get the current item index or set which item we want to play.

---
+++ index.js
@@ -0,0 +1,78 @@
+var clearTracks = function(player) {
+  var remoteTT = player.remoteTextTracks();
+  var i = (remoteTT && remoteTT.length) || 0;
+
+  while (i--) {
+    player.removeRemoteTextTrack(remoteTT[i]);
+  }
+};
+
+var playItem = function(player, obj) {
+  console.log(obj);
+  var i, replay;
+
+  replay = !player.paused();
+
+  player.poster(obj.poster);
+  player.src(obj.sources);
+
+  clearTracks(player);
+
+  i = (obj.textTracks && obj.textTracks.length) || 0;
+  while (i--) {
+    player.addRemoteTextTrack(obj.textTracks[i]);
+  }
+
+  if (replay) {
+    player.play();
+  }
+
+  return player;
+};
+
+var playlistMaker = function(player, plist) {
+  var currentIndex = 0;
+  var playlist = function playlist(list) {
+    this.playlist = playlistMaker(this, list);
+  };
+
+  playlist.list = function list() {
+    return plist.slice();
+  };
+
+  playlist.item = function item(index) {
+    if (index) {
+      currentIndex = 0;
+      return playItem(player, plist[i]);
+    }
+
+    return currentIndex;
+  };
+
+  playlist.next = function next() {
+    var prevIndex = currentIndex;
+    currentIndex = Math.min(currentIndex + 1, plist.length - 1);
+    if (prevIndex === currentIndex) {
+      return;
+    }
+    return playItem(player, plist[currentIndex]);
+  };
+
+  playlist.previous = function previous() {
+    var prevIndex = currentIndex;
+    currentIndex = Math.max(currentIndex - 1, 0);
+    if (prevIndex === currentIndex) {
+      return;
+    }
+    return playItem(player, plist[currentIndex]);
+  };
+
+  return playlist;
+};
+
+var playlist = function playlist(list) {
+  this.playlist = playlistMaker(this, list);
+};
+
+
+videojs.plugin('playlist', playlist);
