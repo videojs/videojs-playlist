7cd39fb3a0aee300154b7e2dfda24203b75323ab
Gary Katsevman <git@gkatsev.com>
2015-02-26 18:48:01 -0500

    Add a player-proxy for tests

---
+++ test/player-proxy.js
@@ -0,0 +1,11 @@
+module.exports = {
+  play: Function.prototype,
+  paused: Function.prototype,
+  ended: Function.prototype,
+  poster: Function.prototype,
+  src: Function.prototype,
+  addRemoteTextTrack: Function.prototype,
+  removeRemoteTextTrack: Function.prototype,
+  remoteTextTracks: Function.prototype,
+  playlist: {}
+};
