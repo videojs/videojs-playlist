fa19b889ec85210cd99f1b5e5d279e23de7cad74
David LaPalomento <dlapalomento@gmail.com>
2015-03-12 15:54:41 -0400

    Initial commit Setup the repo with the yoeman generator. Add basic browserify.

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+    clean: {
+      files: ['dist']
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= browserify.dev.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    qunit: {
+      files: 'test/**/*.html'
+    },
+    jshint: {
+      gruntfile: {
+        options: {
+          node: true
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['lib/**/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src', 'qunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      }
+    },
+    connect: {
+      dev: {
+        options: {
+          hostname: '*',
+          port: 9898,
+          keepalive: true
+        }
+      }
+    },
+
+    browserify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dev: {
+        src: ['lib/videojs-playlist-ui.js'],
+        dest: 'dist/videojs-playlist-ui.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default',
+                     ['clean',
+                      'jshint',
+                      'browserify',
+                      'qunit',
+                      'uglify']);
+};
---
+++ test/videojs-playlist-ui.test.js
@@ -0,0 +1,57 @@
+/*! videojs-playlist-ui - v0.0.0 - 2015-3-12
+ * Copyright (c) 2015 Brightcove
+ * Licensed under the Apache-2.0 license. */
+(function(window, videojs, qunit) {
+  'use strict';
+
+  var realIsHtmlSupported,
+      player,
+
+      // local QUnit aliases
+      // http://api.qunitjs.com/
+
+      // module(name, {[setup][ ,teardown]})
+      module = qunit.module,
+      // test(name, callback)
+      test = qunit.test,
+      // ok(value, [message])
+      ok = qunit.ok,
+      // equal(actual, expected, [message])
+      equal = qunit.equal,
+      // strictEqual(actual, expected, [message])
+      strictEqual = qunit.strictEqual,
+      // deepEqual(actual, expected, [message])
+      deepEqual = qunit.deepEqual,
+      // notEqual(actual, expected, [message])
+      notEqual = qunit.notEqual,
+      // throws(block, [expected], [message])
+      throws = qunit.throws;
+
+  module('videojs-playlist-ui', {
+    setup: function() {
+      // force HTML support so the tests run in a reasonable
+      // environment under phantomjs
+      realIsHtmlSupported = videojs.Html5.isSupported;
+      videojs.Html5.isSupported = function() {
+        return true;
+      };
+
+      // create a video element
+      var video = document.createElement('video');
+      document.querySelector('#qunit-fixture').appendChild(video);
+
+      // create a video.js player
+      player = videojs(video);
+
+      // initialize the plugin with the default options
+      player.playlistUi();
+    },
+    teardown: function() {
+      videojs.Html5.isSupported = realIsHtmlSupported;
+    }
+  });
+
+  test('registers itself', function() {
+    ok(player.playlistUi, 'registered the plugin');
+  });
+})(window, window.videojs, window.QUnit);
