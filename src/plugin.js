7febb40ad35a27f9267d7c5ff982fc3e0f40b29c
Pat O'Neill <pgoneill@gmail.com>
2017-02-10 15:46:07 -0500

    Fix videojs.plugin deprecation warnings

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,9 @@
 import videojs from 'video.js';
 import playlistMaker from './playlist-maker';
 
+// Video.js 5/6 cross-compatible.
+const registerPlugin = videojs.registerPlugin || videojs.plugin;
+
 /**
  * The video.js playlist plugin. Invokes the playlist-maker to create a
  * playlist function on the specific player.
@@ -11,6 +14,6 @@ const plugin = function(list, item) {
   playlistMaker(this, list, item);
 };
 
-videojs.plugin('playlist', plugin);
+registerPlugin('playlist', plugin);
 
 export default plugin;
